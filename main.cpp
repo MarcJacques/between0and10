d6c36dd8db6d24fcaf755e5e522a3513226d9eaa
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-24 18:21:29 +0000

    add conditional statement

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+using namespace std;
+int main() 
+{
+  int x;
+  cin >> x;
 
-int main() {
-  std::cout << "Hello World!\n";
+  if ((x > 0) && (x < 10)) {
+    cout << "This is what were looking for" << endl;
+  } else {
+    cout << "wrong";
+  }
+  return 0;
 }
